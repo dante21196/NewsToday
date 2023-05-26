ecf41cec902a2f262e4487df75e1927c01553881
dante21196 <ashminsingh21196@gmail.com>
2023-05-26 11:04:52 +0530

    NewsToday: Github PaGes refactored

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 import React,{Component} from 'react'
 import News from './components/News'
 import Navbar from './components/Navbar'
-import {
-   BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation
+// import {
+//    BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   useLocation
   
-} from 'react-router-dom'
+// } from 'react-router-dom'
 // import LoadingBar from 'react-top-loading-bar';
-function AppRoutes() {
-  const location = useLocation();
+// function AppRoutes() {
+//   const location = useLocation();
 
-  return (
-    <Routes key={location.pathname}>
-      <Route path="/" element={<News  pagesize={8} country="in" category="general" />} />
-      <Route path="/business" element={<News  pagesize={8} country="in" category="business" />} />
-      <Route path="/entertainment" element={<News  pagesize={8} country="in" category="entertainment" />} />
-      <Route path="/health" element={<News  pagesize={8} country="in" category="health" />} />
-      <Route path="/science" element={<News  pagesize={8} country="in" category="science" />} />
-      <Route path="/sports" element={<News  pagesize={8} country="in" category="sports" />} />
-      <Route path="/technology" element={<News  pagesize={8} country="in" category="technology" />} />
-    </Routes>
-  );
-}
+//   return (
+//     <Routes key={location.pathname}>
+//       <Route path="/" element={<News  pagesize={8} country="in" category="general" />} />
+//       <Route path="/business" element={<News  pagesize={8} country="in" category="business" />} />
+//       <Route path="/entertainment" element={<News  pagesize={8} country="in" category="entertainment" />} />
+//       <Route path="/health" element={<News  pagesize={8} country="in" category="health" />} />
+//       <Route path="/science" element={<News  pagesize={8} country="in" category="science" />} />
+//       <Route path="/sports" element={<News  pagesize={8} country="in" category="sports" />} />
+//       <Route path="/technology" element={<News  pagesize={8} country="in" category="technology" />} />
+//     </Routes>
+//   );
+// }
 
 export default class App extends Component{
   // state={
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import {a} from 'react-router-dom'
 export default class Navbar extends Component {
   const 
   
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
        
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <Link className="mx-3 navbar-brand mx-2" to='/' >NewsToday</Link>
+  <a className="mx-1 navbar-brand mx-2" href='#' >NewsToday</a>
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -16,29 +16,29 @@ export default class Navbar extends Component {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/" >Home</Link>
+        <a className="mx-1 nav-link" href="#" >Home</a>
       </li>
       <li className="nav-item">
-        <Link className="mx-3 nav-link" to="/" >About NewsToday</Link>
+        <a className="mx-1 nav-link" href="#" >About NewsToday</a>
       </li>
       
         <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/business" >Business</Link>
+        <a className="mx-1 nav-link" href="#" >Business</a>
       </li>
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/entertainment" >Entertainment</Link>
+        <a className="mx-1 nav-link" href="#" >Entertainment</a>
       </li>
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/health" >Health</Link>
+        <a className="mx-1 nav-link" href="#" >Health</a>
       </li>
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/general" >General</Link>
+        <a className="mx-1 nav-link" href="#" >General</a>
       </li>
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/science" >Science</Link>
+        <a className="mx-1 nav-link" href="#" >Science</a>
       </li>
       <li className="nav-item active">
-        <Link className="mx-3 nav-link" to="/sports" >Sports</Link>
+        <a className="mx-1 nav-link" href="#" >Sports</a>
       </li>
     </ul>
    
